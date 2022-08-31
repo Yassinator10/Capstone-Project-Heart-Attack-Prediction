47f61bbe3d6301aa8fb19564dcdc5718ec13fecf
Yassine Raouz <rzyacine@hotmail.com>
2022-08-31 15:58:33 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Finding the best mmachine learning model to predict the chances of having a heart attack.',
+    author='Yassine Raouz',
+    license='MIT',
+)
